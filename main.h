d88410565b01e887c53b3b2a442a1dbffcf39dbe
RanaFawzy <rororona91@gmail.com>
2023-09-16 06:02:37 -0700

    add main.h

--- main.h
+++ main.h
@@ -59,3 +59,12 @@ int print_string(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 int flags, int width, int precision, int size);
+/* width handlerrr */
+int handle_write_char(char c, char buffer[],
+int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+int width, int flags, char padd, char extra_c, int padd_start)

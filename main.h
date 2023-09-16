1c20d4a284808fd5ad9fe37d71927eeb623736d7
RanaFawzy <rororona91@gmail.com>
2023-09-16 05:59:30 -0700

    add main.h

--- main.h
+++ main.h
@@ -52,3 +52,10 @@ int print_hexa_upper(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
+/* Funtions to printt chars and stringss */
+int print_char(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+int flags, int width, int precision, int size);

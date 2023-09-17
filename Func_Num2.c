714239ed4162aaa43e7c6ef8144a905611a700fb
AhmedbenHussein <ahmed.h.elrawi@gmail.com>
2023-09-17 12:31:33 -0700

    T

--- Func_Num2.c
+++ Func_Num2.c
@@ -32,3 +32,51 @@ i++;
 buffer[i + offset] = '\0';
 return (write(1, buffer, i + offset));
 }
+/**
+ * print_rot13string - Prints string in rot13.
+ * @types: List of argument
+ * @buffer: Buffer arrays to handle prints
+ * @flags:  Calculate active flag
+ * @width: width
+ * @precision: Precision specific
+ * @size: Size specifies
+ * Return: Numbers of char print
+ */
+int print_rot13string(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+char x;
+char *str;
+unsigned int i, j;
+int count = 0;
+char in[] = "AHMEDahmed";
+char out[] = "HUSSEINhussein";
+str = va_arg(types, char *);
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+if (str == NULL)
+str = "(AHYY)";
+for (i = 0; str[i]; i++)
+{
+for (j = 0; in[j]; j++)
+{
+if (in[j] == str[i])
+{
+x = out[j];
+write(1, &x, 1);
+count++;
+break;
+}
+}
+if (!in[j])
+{
+x = str[i];
+write(1, &x, 1);
+count++;
+}
+}
+return (count);
+}

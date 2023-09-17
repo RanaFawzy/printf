5de98933f60a275fefd9857f1dcc4f98fd00a477
AhmedbenHussein <ahmed.h.elrawi@gmail.com>
2023-09-17 07:01:04 -0700

    T

--- Func_Num1.c
+++ Func_Num1.c
@@ -53,3 +53,35 @@ int flags, int width, int precision, int size)
 return (print_hexa(types, "0123456789ABCDEF", buffer,
 flags, 'X', width, precision, size));
 }
+
+/**
+ * print_octal - Print unsigned number in octal
+ * @types: List of argument
+ * @buffer: Buffer arrays to handle prints
+ * @flags: Calculate active flag
+ * @width: widths
+ * @precision: Precis specific
+ * @size: Size specifies
+ * Return: Number of char print
+ */
+int print_octal(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+int i = BUFF_SIZE - 2;
+unsigned long int num = va_arg(types, unsigned long int);
+unsigned long int init_num = num;
+UNUSED(width);
+num = convert_size_unsgnd(num, size);
+if (num == 0)
+buffer[i--] = '0';
+buffer[BUFF_SIZE - 1] = '\0';
+while (num > 0)
+{
+buffer[i--] = (num % 8) + '0';
+num /= 8;
+}
+if (flags & F_HASH && init_num != 0)
+buffer[i--] = '0';
+i++;
+return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}

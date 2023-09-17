04f7841b9748efa7347d662e5affeb8917202182
RanaFawzy <rororona91@gmail.com>
2023-09-17 06:28:47 -0700

    add get1-sizef.c

--- get1-sizef.c
+++ get1-sizef.c
@@ -21,3 +21,19 @@ else
 *i = curr_i;
 return (size);
 }
+/**
+ * print_hexadecimal - Printss an unsigned number in hexadecimal notation,.
+ * @types: Listaa of argumentsss,.,.
+ * @buffer: Buffer array to handle printtt
+ * @flags:  Calculates active flagsss,.,.,.
+ * @width: get width,,,
+ * @precision: Precision specificationn,,,
+ * @size: Size specifierrr......
+ * Return: Number of char was printed,,,,
+ */
+int print_hexadecimal(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+return (print_hexa(types, "0123456789abcdef", buffer,
+flags, 'x', width, precision, size));
+}

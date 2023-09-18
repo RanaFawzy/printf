4bd775ffc384bee4e14ccd200926788f246b9135
RanaFawzy <rororona91@gmail.com>
2023-09-18 10:40:57 -0700

    add 0utilss.c

---
+++ 0utilss.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * is_printable - Evaluates if a char is a printableee….
+ * @c: Char to be evaluateddd….
+ * Return: 1 if c is printable, 0 otherwise
+ */
+int is_printable(char c)
+{
+if (c >= 32 && c < 127)
+return (1);
+return (0);
+}

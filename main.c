75352c4163cb543df931e56f25b7247f4596585b
AhmedbenHussein <ahmed.h.elrawi@gmail.com>
2023-09-19 04:09:37 -0700

    T

--- main.c
+++ main.c
@@ -41,3 +41,13 @@ _printf("Unknown:[%r]\n");
 printf("Unknown:[%r]\n");
 return (0);
 }
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+_printf("%b\n", 98);
+return (0);
+}
